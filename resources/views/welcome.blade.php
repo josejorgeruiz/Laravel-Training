47496cb2ca8bec07b9022995ee00d2212a969e29
Jose Ruiz <jose.ruiz@vostek.com>
2016-09-04 15:09:15 +0000

    Changed initial view.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,40 +6,10 @@
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
         <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
         </style>
     </head>
     <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
-            </div>
-        </div>
+        <h1>My very first Laravel Application</h1>
+        <p>This is my very first Laravel Application</p>
     </body>
 </html>
