49ad0c732557446021e36740887f39892086fd3e
Jose Ruiz <jose.ruiz@vostek.com>
2016-09-04 16:10:45 +0000

    Fix 1

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>@yield('title')</title>
+  <meta name="description" content="My training">
+  <meta name="author" content="Jose J. Ruiz">
+  <link rel="stylesheet" href="{{ URL::secure('src/css/main.css') }}">
+  @yield('styles')
+</head>
+<body>
+    @include('includes.header')
+    <div class="main">
+        @yield('content')
+    </div>
+</body>
+</html>
