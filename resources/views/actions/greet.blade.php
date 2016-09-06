cf6c3e493d9705c7b3f96a5a544f31dc7b6c2171
Jose Ruiz <jose.ruiz@vostek.com>
2016-09-06 01:28:58 +0000

    Lesson 2

---
+++ resources/views/actions/greet.blade.php
@@ -0,0 +1,9 @@
+
+@extends('layouts.master')
+
+@section('content')
+    <div class='centered'>
+        <a href="{{ route('home') }}">Home</a>
+        <h1>I greet {{ $name === null ? 'you' : $name }}</h1>
+    </div>
+@endsection
