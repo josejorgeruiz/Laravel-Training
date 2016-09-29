745ac6079706d04f062d7476ecb286d7ac6a54b4
Jose Ruiz <jose.ruiz@vostek.com>
2016-09-29 02:16:46 +0000

    S4L15E

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,8 +6,8 @@
   <title>@yield('title')</title>
   <meta name="description" content="My training">
   <meta name="author" content="Jose J. Ruiz">
-  <link rel="stylesheet" href="{{ URL::secure('src/css/main.css') }}">
-  <!-- <link rel="stylesheet" href="{{ URL::to('src/css/main.css') }}"> -->
+  <link rel="stylesheet" href="{{ URL::to('src/css/main.css') }}">
+  <!-- <link rel="stylesheet" href="{{ URL::secure('src/css/main.css') }}"> -->
   @yield('styles')
 </head>
 <body>
